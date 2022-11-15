bb91fb569a864fbf3ebff8905f7cd0e2b6c5ec99
AndresBaquero-00 <andres.baquero6@gmail.com>
2022-11-15 14:17:56 -0500

    Añadido tema de aplicación.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { SigninPage } from "./app/auth";
+import { mainTheme } from "./themes";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signin" element={<SigninPage />} />
+        <ThemeProvider theme={mainTheme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signin" element={<SigninPage />} />
 
-                {/* Redireccionamiento para rutas no existentes. */}
-                <Route path="/*" element={<Navigate to="/signin"/>} />
-            </Routes>
-        </BrowserRouter>
+                    {/* Redireccionamiento para rutas no existentes. */}
+                    <Route path="/*" element={<Navigate to="/signin"/>} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
---
+++ src/themes/index.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const mainTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#F63E02'
+        },
+        secondary: {
+            main: '#F3B700'
+        },
+    }
+});
